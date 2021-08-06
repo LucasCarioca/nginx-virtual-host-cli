ff15edbbb182647688b1247cf9e7952cd11f83a0
Lucas Desouza <lucasfds2777@gmail.com>
2021-08-06 14:26:03 -0500

    allows default for path

--- pkg/controllers/server.go
+++ pkg/controllers/server.go
@@ -11,17 +11,25 @@ func NewServerController() ServerController {
 	return ServerController{}
 }
 
-func check(e error) {
+func checkErr(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
+func getEnv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
+}
+
 func (c *ServerController) Create(serverName string, hostName string) {
-	sitesAvailableRoot := os.Getenv("PROXY_SITES_AVAILABLE")
+	sitesAvailableRoot := getEnv("PROXY_SITES_AVAILABLE", "/etc/nginx/sites-available")
 	fmt.Printf("creating %s -> %s...\n", serverName, hostName)
 	f, err := os.Create(fmt.Sprintf("%s/%s", sitesAvailableRoot, serverName))
-	check(err)
+	checkErr(err)
 	defer f.Close()
 	f.Write([]byte(fmt.Sprintf(SERVER, serverName, hostName)))
 	f.Sync()

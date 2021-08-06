63dc604fb16d2e268bf49f86077b6d1aa0276596
Lucas Desouza <lucasfds2777@gmail.com>
2021-08-06 14:36:50 -0500

    adding version flag

--- main.go
+++ main.go
@@ -7,13 +7,23 @@ import (
 	"os"
 )
 
+var (
+	version   string
+)
+
 func main() {
 	var serverName string
+	var checkVersionFlag bool
 	var hostName string
 	app := &cli.App{
 		Name:  "create",
 		Usage: "Create a new reverse proxy",
 		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:        "version",
+				Usage:       "Get current cli version",
+				Destination: &checkVersionFlag,
+			},
 			&cli.StringFlag{
 				Name:        "name",
 				Aliases:     []string{"n"},
@@ -30,6 +40,10 @@ func main() {
 			},
 		},
 		Action: func(c *cli.Context) error {
+			if checkVersionFlag {
+				println(version)
+				os.Exit(0)
+			}
 			servers := controllers.NewServerController()
 			servers.Create(serverName, hostName)
 			return nil

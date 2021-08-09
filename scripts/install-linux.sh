4210014c18ce28827824dc2a8975c84377fa8de2
Lucas Desouza <lucasfds2777@gmail.com>
2021-08-08 23:01:26 -0500

    update install scripts

--- scripts/install-linux.sh
+++ scripts/install-linux.sh
@@ -1,5 +1,5 @@
 #!/etc/bin/sh
 
-curl -L https://github.com/LucasCarioca/proxy-reverse-proxy-cli/releases/download/v0.1.2/proxy_0.1.2_Linux_x86_64.tar.gz -o proxy.tar.gz
+curl -L https://github.com/LucasCarioca/proxy-reverse-proxy-cli/releases/download/v0.2.0/proxy_0.2.0_Linux_x86_64.tar.gz -o proxy.tar.gz
 tar -xzf proxy.tar.gz
 sudo mv proxy /usr/local/bin/proxy
